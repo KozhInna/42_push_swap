bf8afba7b61a8299cb74c94028a8bf4d1fa4fe31
Inna Kozhina <inna.kozhina22@gmail.com>
2025-03-05 12:41:41 +0200

    refactor sort_five for conciseness

--- src/sort_five_or_less.c
+++ src/sort_five_or_less.c
@@ -6,7 +6,7 @@
 /*   By: ikozhina <ikozhina@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 09:19:53 by ikozhina          #+#    #+#             */
-/*   Updated: 2025/03/05 10:22:55 by ikozhina         ###   ########.fr       */
+/*   Updated: 2025/03/05 12:35:19 by ikozhina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,40 @@ void	sort_five_or_less(t_stack *list, int capacity)
 
 void	sort_five(t_stack *list, int capacity)
 {
-	int	min;
-	int	i;
 	int	len_b;
 
-	i = 0;
-	while (list->length_a > 3)
-	{
-		min = find_min_a(list, capacity);
-		if (list->stack_a[(list->a_start + i) % capacity] == min)
-		{
-			while (list->stack_a[list->a_start] != min)
-				rotate_a(list, capacity);
-			push_b(list, capacity);
-			i = 0;
-		}
-		else if (list->stack_a[(list->a_end - i + capacity) % capacity] == min)
-		{
-			while (list->stack_a[list->a_start] != min)
-				rrotate_a(list, capacity);
-			push_b(list, capacity);
-			i = 0;
-		}
-		else
-			i++;
-	}
+	push_nearest_min_to_b(list, capacity);
 	sort_three(list, capacity);
 	len_b = list->length_b;
 	while (len_b > 0)
 	{
 		push_a(list, capacity);
 		len_b--;
+	}
+}
+void	push_nearest_min_to_b(t_stack *list, int capacity)
+{
+	int i;
+	int min;
+
+	while (list->length_a > 3)
+	{
+		i = 0;
+		min = find_min_a(list, capacity);
+		while (list->stack_a[(list->a_start + i) % capacity] != min &&
+			list->stack_a[(list->a_end - i + capacity) % capacity] != min)
+			i++;
+		if (list->stack_a[(list->a_start + i) % capacity] == min)
+		{
+			while (list->stack_a[list->a_start] != min)
+				rotate_a(list, capacity);
+		}
+		else if (list->stack_a[(list->a_end - i + capacity) % capacity] == min)
+		{
+			while (list->stack_a[list->a_start] != min)
+				rrotate_a(list, capacity);
+		}
+		push_b(list, capacity);
 	}
 }
 
@@ -80,47 +83,3 @@ void	sort_three(t_stack *list, int capacity)
 		rrotate_a(list, capacity);
 }
 
-
-void	sort_hundred(t_stack *list, int capacity)
-{
-	int	point;
-	int	i;
-	int	len_a;
-	int	max;
-
-	while (list->length_a > 3)
-	{
-		point = find_min_a(list, capacity) + (find_max_a(list, capacity) - find_min_a(list, capacity)) / 3;
-		len_a = list->length_a;
-		while (len_a > 0)
-		{
-			if (list->stack_a[list->a_start] <= point)
-				push_b(list, capacity);
-			else
-				rotate_a(list, capacity);
-			len_a--;
-		}
-	}
-	sort_three(list, capacity);
-	i = 0;
-	while (list->length_b > 0)
-	{
-		max = find_max_b(list, capacity);
-		if (list->stack_b[(list->b_start + i) % capacity] == max)
-		{
-			while (list->stack_b[list->b_start] != max)
-				rotate_b(list, capacity);
-			push_a(list, capacity);
-			i = 0;
-		}
-		else if (list->stack_b[(list->a_end - i + capacity) % capacity] == max)
-		{
-			while (list->stack_b[list->b_start] != max)
-				rrotate_b(list, capacity);
-			push_a(list, capacity);
-			i = 0;
-		}
-		else
-			i++;
-	}
-}
